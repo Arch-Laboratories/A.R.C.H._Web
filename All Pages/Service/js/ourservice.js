071eab7a49f316d7df9c738ebb17f300e1c0561c
Abdul Moiz <abdulmoiznoman08@gmail.com>
2025-08-15 08:07:58 +0500

    changes

--- All Pages/Service/js/ourservice.js
+++ All Pages/Service/js/ourservice.js
@@ -25,3 +25,10 @@ document.querySelectorAll(".home-nav a").forEach(link => {
     nav.classList.remove("active");
   });
 });
+
+
+
+
+document.querySelector('.mic-icon').addEventListener('click', function() {
+  this.classList.toggle('active');
+});
